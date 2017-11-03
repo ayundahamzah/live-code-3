3246c9194c16bb91dd9f752d37349e0fcf9d1d98
Ayunda <ayundahamzah@gmail.com>
2017-11-03 09:40:49 +0700

    sesi 1

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-//your code here
+const express = require('express')
+const app = app.express()
+const bodyParser = require('body-parser')
+
+app.set('view','./views')
+app.set('view engine', 'ejs');
+
+
+
+
+app.get('/movies',function(req,res){
+  res.render('movies')
+})
+
+app.get('/prodHouse',),function(req,res){
+  res.render('productionHouse')
+})
+
+
+
+
+
+
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
