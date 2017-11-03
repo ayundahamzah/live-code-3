e14e4e947dee7fa3a1e484a302e8a702adeaf542
Ayunda <ayundahamzah@gmail.com>
2017-11-03 10:33:37 +0700

    sesi 2

--- app.js
+++ app.js
@@ -1,27 +1,36 @@
-const express = require('express')
-const app = app.express()
-const bodyParser = require('body-parser')
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('db/movie.db');
 
-app.set('view','./views')
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+
+app.set('views','./views')
 app.set('view engine', 'ejs');
 
-
-
-
-app.get('/movies',function(req,res){
-  res.render('movies')
+app.get('/movies', function(req,res) {
+db.all(`SELECT * FROM Movies
+  LEFT JOIN ProductionHouse ON Movies.id=ProductionHouse.id `,function(err,rowsmovie,rowsPH){
+    res.render('movie',{rowsmovie},{rowsPH});
+  })
 })
 
-app.get('/prodHouse',function(req,res){
-  res.render('productionHouse')
+app.get('/movies/edit/:id',function(req,res){
+  db.each(``)
+
+})
+
+app.get('/prodHouses', function(req,res){
+  db.all(`SELECT * FROM ProductionHouse`,function(err,rows){
+    res.render('prodHouse',{rows});
+  })
 })
 
 
 
-
-
-
-
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+app.listen(3000, function() {
+  console.log(`Are you looking for me? 3000`);
 })
